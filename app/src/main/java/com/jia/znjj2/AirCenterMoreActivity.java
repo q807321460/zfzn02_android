b6dd9e8ca2fd5a7908532757fab08482b572c413
LuoJuan <2402216934@qq.com>
2018-01-17 16:15:42 +0800

    no update

--- app/src/main/java/com/jia/znjj2/AirCenterMoreActivity.java
+++ app/src/main/java/com/jia/znjj2/AirCenterMoreActivity.java
@@ -5,11 +5,15 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jia.util.NetworkUtil;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class AirCenterMoreActivity extends ElectricBase implements View.OnClickListener {
     private TextView tvTitleName;
@@ -80,12 +84,15 @@ public class AirCenterMoreActivity extends ElectricBase implements View.OnClickL
     private CheckBox cb0413;
     private CheckBox cb0414;
     private CheckBox cb0415;
-    String  str=null;
+    String  str="";
     String  strNumber=null;
     String  strPlus=null;
     String order=null;
-    int i=0;
+    int count;
     private Button btOpen;
+    private TreeMap<String,Integer> checkedStr;
+    //操作取消一个时，全选取消，这个变量是是否是用户点击
+    private boolean checkFoUser=true;
 
 
 
@@ -117,11 +124,17 @@ public class AirCenterMoreActivity extends ElectricBase implements View.OnClickL
         cbCheckAll= (CheckBox) findViewById(R.id.air_center_more_selectall);
         cbCheckAll.setOnCheckedChangeListener(listener);
         cb0100= (CheckBox) findViewById(R.id.cb_100);
+        cb0100.setOnCheckedChangeListener(listener);
         cb0101= (CheckBox) findViewById(R.id.cb_101);
+        cb0101.setOnCheckedChangeListener(listener);
         cb0102= (CheckBox) findViewById(R.id.cb_102);
+        cb0102.setOnCheckedChangeListener(listener);
         cb0103= (CheckBox) findViewById(R.id.cb_103);
+        cb0103.setOnCheckedChangeListener(listener);
         cb0104= (CheckBox) findViewById(R.id.cb_104);
+        cb0104.setOnCheckedChangeListener(listener);
         cb0105= (CheckBox) findViewById(R.id.cb_105);
+        cb0105.setOnCheckedChangeListener(listener);
         cb0106= (CheckBox) findViewById(R.id.cb_106);
         cb0107= (CheckBox) findViewById(R.id.cb_107);
         cb0108= (CheckBox) findViewById(R.id.cb_108);
@@ -180,6 +193,7 @@ public class AirCenterMoreActivity extends ElectricBase implements View.OnClickL
         cb0413= (CheckBox) findViewById(R.id.cb_413);
         cb0414= (CheckBox) findViewById(R.id.cb_414);
         cb0415= (CheckBox) findViewById(R.id.cb_415);
+        checkedStr=new TreeMap<String, Integer>();
         btOpen= (Button) findViewById(R.id.rb_open);
         btOpen.setOnClickListener(this);
 
@@ -196,39 +210,55 @@ public class AirCenterMoreActivity extends ElectricBase implements View.OnClickL
             switch(buttonView.getId())
             {
                 case R.id.air_center_more_selectall:
-                    if(isChecked)
+                    if(isChecked){
+
+                    }
 
                     break;
                 case R.id.cb_100:
-                    if(isChecked)
-                          str=str+"0100";
-                    strPlus=
-                    i++;
+                    if(isChecked){
+                    checkedStr.put("0100",1);}
+                    else {
+                        checkedStr.remove("0100");
+                    }
                     break;
                 case R.id.cb_101:
-                    if(isChecked)
-                        str=str+"0101";
-                    i++;
+                    if(isChecked){
+                        checkedStr.put("0101",2);
+                    }else {
+                            checkedStr.remove("0101");
+                    }
                     break;
                 case R.id.cb_102:
-                    if(isChecked)
-                        str=str+"0102";
-                    i++;
+                    if(isChecked){
+                        checkedStr.put("0102",3);
+                   }else {
+                        if(!str.equals("")){
+                            checkedStr.remove("0102");
+                        }
+
+                    }
                     break;
                 case R.id.cb_103:
-                    if(isChecked)
-                        str=str+"0103";
-                    i++;
+                    if(isChecked) {
+                        checkedStr.put("0103",4);
+                    }else {
+                            checkedStr.remove("0103");
+                    }
                         break;
                 case R.id.cb_104:
-                    if(isChecked)
-                        str=str+"0104";
-                    i++;
+                    if(isChecked) {
+                        checkedStr.put("0104",5);
+                    }else {
+                        checkedStr.remove("0104");
+                    }
                         break;
                 case R.id.cb_105:
-                    if(isChecked)
-                        str=str+"0105";
-                    i++;
+                    if(isChecked) {
+                        checkedStr.put("0105",6);
+                    }else {
+                        checkedStr.remove("0105");
+                    }
                         break;
                 case R.id.cb_106:
                     if(isChecked)
@@ -467,19 +497,62 @@ public class AirCenterMoreActivity extends ElectricBase implements View.OnClickL
             }
         }
     };
-
+    public String getString(String s, String s1)//s是需要删除某个子串的字符串,s1是需要删除的子串
+    {
+        int postion = s.indexOf(s1);
+        int length = s1.length();
+        int Length = s.length();
+        String newString = s.substring(0,postion) + s.substring(postion + length, Length);
+        return newString;//返回已经删除好的字符串
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.rb_open:
-                if(i<10){
-                   strNumber= "0"+String.valueOf(i);
-                }else {
-                    strNumber=String.valueOf(i);
+                int i=checkedStr.size();
+                strNumber= String.valueOf(i);
+                Set<String> set = checkedStr.keySet();
+                for (String key:set) {
+                    System.out.println("第一种："+checkedStr.get(key));
                 }
-               order="013101"+strNumber+str+
-
+                count=count+Integer.parseInt(strNumber,16)+Integer.parseInt("33",16);
+                int mod = count % 256;
+                String hex = Integer.toHexString(mod);
+                int len = hex.length();
+                // 如果不够校验位的长度，补0,这里用的是两位校验
+                if (len < 2) {
+                    hex = "0" + hex;
+                }
+                order="013101"+strNumber+str+hex;
+                if(order.length()>13) {
+                    open(order);
+                }
                 break;
+            default:
+
+        }
+    }
+    public void init2(){
+        str="";
+        count=0;
+        order="";
+    }
+    public void open(final String order2){
+        if (mDC.socketCrash || mDC.bIsRemote || checkNetConnection()){
+            //if (true){
+            new Thread(){
+                @Override
+                public void run() {
+                    mDC.mWS.updateElectricOrder(mDC.sMasterCode,electric.getElectricCode(),""+mDC.orderSign+mDC.airSet,order2);
+                    System.out.println("远程开电器：");
+                }
+            }.start();
+
+        }else{
+            //本地socket通信
+            String order1 = "<" + electric.getElectricCode()+mDC.orderSign+mDC.airSet + order2 + "FF"+">";
+            System.out.println("本地开电气： "+order1);
+            NetworkUtil.out.println(order1);
         }
     }
 }
